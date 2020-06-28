9c3bb20319a0036822cb7cb82fe46c6f601a3807
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-06-28 19:27:21 -0400

    dependencia específica implementada

---
+++ WebContent/res/js/accidentes.js
@@ -0,0 +1,23 @@
+$(function() {
+    // Al hacer click sobre uno de los botones (ver, editar, borrar)
+    $('[data-do]').on('click', function(event) {
+        // Prevenir acción por defecto
+        event.preventDefault();
+
+        // Recuperar ID del Accidente seleccionado
+        let id = $(this).data('id');
+
+        // Recuperar la acción a realizar
+        let accion = $(this).data('do');
+
+        // Confirmar eliminación del registro
+        if(accion === 'borrar') {
+            if(confirm('¿Está seguro de querer eliminar el registro del accidente?')) {
+                $(location).attr('href', '?do=borrar&id=' + id);
+                return true;
+            }
+        }
+
+        $(location).attr('href', '?do=' + accion + (id !== undefined ? '&id=' + id : ''));
+    });
+});
