0d4b2bf0f9fa16ab76825b039ad9f9bfc8a56585
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-06-28 22:14:34 -0400

    agregados los registros iniciales de las tablas

---
+++ Data Manipulation Language.sql
@@ -0,0 +1,65 @@
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('61',to_date('02/01/19','DD/MM/RR'),'22:51','4287 Raven Park','Fresnes','Expanded client-driven approach','Badeau','LEVE','TRABAJO','TESTIGOS','Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.
+
+Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('62',to_date('20/04/20','DD/MM/RR'),'09:42','1 Arapahoe Court','Kraljevica','Secured bandwidth-monitored encryption','Colorado','LEVE','TRAYECTO','OTRO','Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
+
+In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
+
+Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('63',to_date('11/08/19','DD/MM/RR'),'09:50','74854 Mandrake Plaza','Simpangpasir','Total multimedia support','Oak Valley','FATAL','TRAYECTO','PARTE','Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.
+
+Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('64',to_date('23/05/19','DD/MM/RR'),'15:51','123 Hanson Place','Dunkerque','Vision-oriented uniform service-desk','Cascade','GRAVE','TRAYECTO','TESTIGOS','Phasellus in felis. Donec semper sapien a libero. Nam dui.
+
+Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('65',to_date('19/07/19','DD/MM/RR'),'13:14','933 Southridge Road','Prengtale','Reverse-engineered 24/7 parallelism','Johnson','LEVE','TRABAJO','TESTIGOS','Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
+
+Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.
+
+Fusce consequat. Nulla nisl. Nunc nisl.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('66',to_date('18/11/19','DD/MM/RR'),'10:13','82 Elmside Lane','Novotitarovskaya','Horizontal actuating focus group','Mayer','OTRO','TRAYECTO','PARTE','Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
+
+In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
+
+Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('67',to_date('18/12/20','DD/MM/RR'),'20:18','658 Westend Hill','VarazqÄ�n','Self-enabling human-resource circuit','Cordelia','OTRO','TRAYECTO','DECLARACION','Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.
+
+In congue. Etiam justo. Etiam pretium iaculis justo.
+
+In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('68',to_date('26/06/19','DD/MM/RR'),'20:55','2 Green Ridge Street','Inari','Exclusive zero administration infrastructure','Norway Maple','LEVE','TRAYECTO','PARTE','In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.
+
+Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+
+Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('69',to_date('13/11/20','DD/MM/RR'),'20:09','4664 Mendota Terrace','Aleksandrovka','Optimized demand-driven paradigm','Chive','OTRO','TRABAJO','TESTIGOS','In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.
+
+Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('70',to_date('04/12/19','DD/MM/RR'),'18:41','73 Calypso Plaza','Realeza','Monitored optimal concept','Swallow','GRAVE','TRABAJO','OTRO','Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+
+Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
+
+Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('71',to_date('08/11/20','DD/MM/RR'),'12:46','7 Sage Point','Lipiany','Innovative system-worthy synergy','Montana','LEVE','TRABAJO','OTRO','Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+
+Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+
+In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('72',to_date('20/11/19','DD/MM/RR'),'18:03','1559 Roth Way','Krajan Puger Wetan','Digitized needs-based neural-net','Blue Bill Park','GRAVE','TRABAJO','DECLARACION','Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+
+In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('73',to_date('12/04/20','DD/MM/RR'),'17:49','94371 6th Drive','AnastÃ¡cio','Ameliorated fault-tolerant open architecture','Forster','OTRO','TRABAJO','OTRO','Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.
+
+Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('74',to_date('13/09/20','DD/MM/RR'),'09:46','4 Dahle Crossing','Mozirje','Switchable dedicated monitoring','Pennsylvania','FATAL','TRABAJO','DECLARACION','Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
+
+In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.
+
+Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.');
+Insert into SYSTEM.ACCIDENTES (ID,FECHA,HORA,DIRECCION,COMUNA,CIRCUNSTANCIA,LUGAR,CLASIFICACION,TIPO,MEDIO_PRUEBA,DETALLES) values ('75',to_date('16/11/19','DD/MM/RR'),'20:31','9492 Moulton Park','Sung Noen','Intuitive discrete analyzer','5th','FATAL','TRABAJO','TESTIGOS','Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
+
+In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.
+
+Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.');
