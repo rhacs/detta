d9fdbe3c76c708129afe4c19c1d0bd852bb7f222
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-07-01 16:55:12 -0400

    repositorio IEmpresasRepository implementado

---
+++ src/cl/rhacs/detta/repositorios/interfaces/IEmpresasRepository.java
@@ -0,0 +1,27 @@
+package cl.rhacs.detta.repositorios.interfaces;
+
+import java.util.List;
+
+import cl.rhacs.detta.modelos.Empresa;
+
+public interface IEmpresasRepository extends IRepository {
+
+    /**
+     * Busca en el repositorio por el registro con el rut especificado
+     * 
+     * @param rut rut a buscar
+     * @return un objeto {@link Empresa} con el resultado, {@code null} en cualquier
+     *         otro caso
+     */
+    public Empresa buscarPorRut(String rut);
+
+    /**
+     * Busca en el repositorio las {@link Empresa}s que tengan el giro especificado
+     * 
+     * @param giro giro a buscar
+     * @return un objeto {@link List} con los resultados obtenidos, {@code null} en
+     *         cualquier otro caso
+     */
+    public List<Empresa> buscarPorGiro(String giro);
+
+}
